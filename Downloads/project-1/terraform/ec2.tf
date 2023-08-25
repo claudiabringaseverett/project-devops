2bb07ece598c46aa6491a931ddeaec98d0c6e036
claudiabringaseverett <claudiaeverett9@gmail.com>
2023-08-25 00:03:56 -0400

    adding jenkins master and build slave

--- Downloads/project-1/terraform/ec2.tf
+++ Downloads/project-1/terraform/ec2.tf
@@ -1,23 +1,31 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "jenkins-server"{
-    ami = "ami-0261755bbcb8c4a84"
-    instance_type = "t2.micro"
-    key_name = "new_key"
+resource "aws_instance" "demo-server" {
+  ami           = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.micro"
+  key_name      = "new_key"
+  vpc_security_group_ids = [ aws_security_group.demo-sg.id ]
+#   security_groups = [ "demo-sg" ]
+  subnet_id = aws_subnet.demo-public-subnet-01.id
+  for_each = toset([ "Jenkins-master", "build-slave"])
+  tags = {
+    Name = "${each.key}"
+  }
 }
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
+  vpc_id = aws_vpc.demo-vpc.id
 
   ingress {
-    description      = "SSH Access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -31,4 +39,51 @@ resource "aws_security_group" "demo-sg" {
   tags = {
     Name = "ssh-port"
   }
+}
+
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_subnet" "demo-public-subnet-01" {
+  vpc_id           = aws_vpc.demo-vpc.id
+  cidr_block       = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "demo-public-subnet-01"
+  }
+}
+
+resource "aws_subnet" "demo-public-subnet-02" {
+  vpc_id           = aws_vpc.demo-vpc.id
+  cidr_block       = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "demo-public-subnet-02"
+  }
+}
+
+resource "aws_internet_gateway" "demo-igw" {
+  vpc_id = aws_vpc.demo-vpc.id
+  tags = {
+    Name = "demo-igw"
+  }
+}
+
+resource "aws_route_table" "demo-public-rt" {
+    vpc_id = aws_vpc.demo-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.demo-igw.id
+    }
+}
+
+resource "aws_route_table_association" "demo-rta-public-subnet-01" {
+    subnet_id = aws_subnet.demo-public-subnet-02.id
+    route_table_id = aws_route_table.demo-public-rt.id
 }
